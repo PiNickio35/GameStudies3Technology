e5199e2ee7431676a9662e5160f246de66aa00f9
Nicolaas du Toit van der Merwe <nicolaasvdmerwe0@gmail.com>
2025-10-28 21:17:04 +0200

    Basic UI
    Just a main menu and instructions. UI in the main game is still lacking.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,10 +1,28 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private Button instructions;
+    
     public void StartGame()
     {
         SceneManager.LoadSceneAsync(1);
+    }
+
+    public void Instructions()
+    {
+        instructions.gameObject.SetActive(true);
+    }
+
+    public void HideInstructions()
+    {
+        instructions.gameObject.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
